436c82dd735bbb1f8079adf5d97346df4fa0d041
laurelomozusi25 <laurelomozusi.25@gmail.com>
2026-05-31 10:07:25 +0100

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,22 +1,32 @@
-const CACHE = 'medsidekick-v2';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'medsidekick-v3';
+const ASSETS = [
+    '/Medsidekick/',
+    '/Medsidekick/index.html',
+    '/Medsidekick/manifest.json'
+];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+    e.waitUntil(
+        caches.open(CACHE).then(c => c.addAll(ASSETS))
+    );
     self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(keys =>
-        Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ));
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        )
+    );
     self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
     e.respondWith(
-        caches.match(e.request).then(cached => 
-            cached || fetch(e.request).catch(() => caches.match('/index.html'))
+        caches.match(e.request).then(cached =>
+            cached || fetch(e.request).catch(() =>
+                caches.match('/Medsidekick/index.html')
+            )
         )
     );
 });
